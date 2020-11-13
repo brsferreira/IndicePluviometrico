50e4ace3adbd6ba1eb13c2176d9ebb5f08e9e3c4
brunodev4u <bsf.6626@gmail.com>
2020-11-13 13:29:39 -0300

    correcao de calculo

--- src/indicePluviometrico/Aplicacao.java
+++ src/indicePluviometrico/Aplicacao.java
@@ -4,23 +4,15 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Pluviosidade p1 = new Pluviosidade(1000, 1);
+		double indicePluviometrico = 1.00; //mm
+		double areaTotal = 10.00; //m²
+		int periodo = 2; //horas
+		
+		Pluviosidade p1 = new Pluviosidade(indicePluviometrico, areaTotal, periodo);
 		p1.Calculapluviosidade();
 		
 		
-		//StringBuffer sb = new StringBuffer(); // thread-safe - use no contexto de concorrência
-				StringBuilder sb = new StringBuilder(); // não-thread-safe - mais rápido
-				
-				sb.append("Bem vindo ao curso de Java. ");
-				sb.append("Fique a vontade para tirar suas dúvidas. ");
-				sb.append("Estamos aqui para te ajudar. ");
-				
-				System.out.println(sb.toString());
-				
-				sb.insert(26, " da AlgaWorks");
-				
-				System.out.println(sb);
 
-	}//main -------------------------------------
+	}//main
 
-}//class ----------------------------------------
+}//class
--- src/indicePluviometrico/Pluviosidade.java
+++ src/indicePluviometrico/Pluviosidade.java
@@ -1,5 +1,7 @@
 package indicePluviometrico;
 
+import java.util.Locale;
+
 /*O que é (significado)
 Pluviosidade é o volume (quantidade) de chuvas, que ocorre numa determinada área (cidade, bairro ou região, por exemplo) num determinado período de tempo (dia, mês ou ano).
 A pluviosidade é um componente muito importante na composição do clima de uma região. Ela tem grande influência nas temperaturas e índices de umidade do ar das regiões.
@@ -11,72 +13,75 @@ O índice pluviométrico (pluviosidade de uma área) é calculado com um instrumento
 
 FATOR DE CONVERSAO: 1m³ = 1000L, 1dm³ = 1L, 1cm³ (lê-se um centímetro cúbico) = 1 mililitro (ml) 
 
-1mmm = 1L por m²
+1mm = 1L por m²
 
 */
 
 public class Pluviosidade {
-	
-	private double indice; //altura em milimetros no medidor.
+
+	private double indice; // altura em milimetros no medidor.
 	private double areaTotal;
 	private double volume;
-	private int periodo;
-	
+	private int periodo; // horas
+
 	// Construtor -------------------------------
-	
-	public Pluviosidade(double indice, double areaTotal) {
+
+	public Pluviosidade(double indice, double areaTotal, int periodo) {
 		super();
 		this.indice = indice;
 		this.areaTotal = areaTotal;
+		this.periodo = periodo;
 	}
-	
+
 	// Acessores --------------------------------
 	public double getIndice() {
 		return indice;
 	}
+
 	public void setIndice(double indice) {
 		this.indice = indice;
 	}
+
 	public double getVolume() {
 		return volume;
 	}
+
 	public void setVolume(double volume) {
 		this.volume = volume;
 	}
+
 	public double getAreaTotal() {
 		return areaTotal;
 	}
+
 	public void setAreaTotal(double areaTotal) {
 		this.areaTotal = areaTotal;
 	}
+
 	public int getPeriodo() {
 		return periodo;
 	}
+
 	public void setPeriodo(int periodo) {
 		this.periodo = periodo;
 	}
-	
-	
+
 	public void Calculapluviosidade() {
-		
-		double indiceEmMetros = (this.indice)/1000; //converte o indice para metros.
-		double volumeMetroCubico = (indiceEmMetros)*(this.areaTotal); //calcula o volume em M³
-		System.out.printf("%n%s%.3f%s%n", "Volume em metros cúbicos: ", volumeMetroCubico, " m³");
-		
-		double volumeLitros = (volumeMetroCubico)*1000;
+
+		Locale.setDefault(Locale.US);
+
+		double indiceEmMetros = (indice) / 1000; // converte o indice para metros.
+		double volumeMetroCubico = (indiceEmMetros) * (areaTotal); // calcula o volume em M³
+		System.out.printf("%n%s%.2f%s%.3f%s%n", "IndicePluviometrico(mm): ", indice, " mm = ", indiceEmMetros,
+				" m");
+
+		double volumeLitros = (volumeMetroCubico) * 1000;
 		System.out.printf("%n%s%.2f%s%n", "Volume em litros: ", volumeLitros, " litros por m²");
+
+		// Total de litros recebidos na área
+		double volumeTotal = volumeLitros * areaTotal * periodo;
+		System.out.printf("%n%s%.2f%s%d%s%n", "Volume Total na area = ", volumeTotal, " litros durante o periodo de ",
+				periodo, " h");
 	}
-	
-	
 
-}//class ----------------------------------------
-
-
-
-
-
-
-
-
-
-
+}// class ----------------------------------------
